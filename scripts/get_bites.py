32f0c029b6074616e5b33666558dd27a51667852
SJohnson <simonbjohnson@gmail.com>
2021-02-03 00:43:06 +0000

    script to interact with hxl bites server

---
+++ scripts/get_bites.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+data = {'hxlData' : [['organisation','reached'],['#org','#reached'],['BRC',20],['ARC',30]]}
+
+
+r = requests.post('http://127.0.0.1:3000/charts', json=data)
+#r = requests.post('http://httpbin.org/post', json=data)
+
+print(r.status_code)
+
+print(r.json())

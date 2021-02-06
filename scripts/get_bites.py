f33b6768bf6a74d42a64047bd04909e8efe96b4f
SJohnson <simonbjohnson@gmail.com>
2021-02-06 15:23:55 +0000

    testin hxlbites server

--- scripts/get_bites.py
+++ scripts/get_bites.py
@@ -10,3 +10,19 @@ r = requests.post('http://127.0.0.1:3000/charts', json=data)
 print(r.status_code)
 
 print(r.json())
+
+
+r = requests.post('http://127.0.0.1:3000/maps', json=data)
+#r = requests.post('http://httpbin.org/post', json=data)
+
+print(r.status_code)
+
+print(r.json())
+
+
+r = requests.post('http://127.0.0.1:3000/text', json=data)
+#r = requests.post('http://httpbin.org/post', json=data)
+
+print(r.status_code)
+
+print(r.json())
